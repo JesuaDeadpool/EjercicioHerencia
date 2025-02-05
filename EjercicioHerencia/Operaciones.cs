d2c19a29aea847698936dda82a347390a87ef837
Jesua <144072667+JesuaDeadpool@users.noreply.github.com>
2025-02-04 23:33:45 -0600

    Creacion de la clase Padre Operaciones y la implementacion de los tres metodos con su respectiva formula

---
+++ EjercicioHerencia/Operaciones.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioHerencia
+{
+    public class Operaciones
+    {
+
+        public double AreaTrapeacio(double baseA, double ancho, double altura)
+        {
+            return ((baseA + ancho) * altura) / 2; 
+        }
+
+
+        public double VolumenEsfera(double radio)
+        {
+            return 4 / 3 * (Math.PI * Math.Pow(radio, 3));
+
+        }
+
+        public double LongitudCircuferencia(double radio)
+        {
+            return 2 * Math.PI * radio; 
+        }
+
+
+    }
+}
